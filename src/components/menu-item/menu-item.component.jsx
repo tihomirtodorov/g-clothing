4114fff37a102b9fca0350962a084ec614edc54f
tihomirtodorov <tihomir@zoomd.com>
2021-02-12 22:50:05 +0200

    Added routing

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,16 +1,23 @@
 import React from 'react';
 import './menu-item.styles.scss';
+import { withRouter } from 'react-router-dom';
 
-const MenuItem = ({ title, imageUrl, size }) => (
-  <div className={`${size} menu-item`}>
-    <div className='background-image' style={{
-      backgroundImage: `url(${imageUrl})`
-    }}/>
-    <div className='content'>
-      <h1 className='title'>{title.toUpperCase()}</h1>
-      <span className='subtitle'>SHOP NOW</span>
+const MenuItem = ({ title, imageUrl, size, linkUrl, history, match }) => {
+  console.log(`${match.url}${linkUrl}`)
+  return (
+    <div 
+      className={`${size} menu-item`} 
+      onClick={() => history.push(`${match.url}${linkUrl}`)}
+    >
+      <div className='background-image' style={{
+        backgroundImage: `url(${imageUrl})`
+      }}/>
+      <div className='content'>
+        <h1 className='title'>{title.toUpperCase()}</h1>
+        <span className='subtitle'>SHOP NOW</span>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
-export default MenuItem;
+export default withRouter(MenuItem);
